020b69d10f5675b6b642a5c409fe95aea17d3851
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-11-01 09:49:46 +0200

    imported form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDisplayComponent } from './quote-display/quote-display.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DaysCountPipe } from './days-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDisplayComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    DaysCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
