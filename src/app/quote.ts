2c994d445eb2c1d4236b9bdcce6f180a8c3f039f
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-11-01 09:53:20 +0200

    initial value for object

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,7 @@
 export class Quote {
+    showForm:boolean;
+    constructor(public id:number,public name:string,public quotes: string,public datePosted: Date, 
+      public numberOfLikes:number,public numberOfDislikes:number){
+        this.showForm=false;
+    }
 }
